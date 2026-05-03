8c3f7c9b7a5bae7a1234fa8ca800df755a1d9a91
oscarlmjr <oscarlmjr@gmail.com>
2026-05-03 09:32:49 -0300

    secao_5 aula_94-95

--- 3_conn_containers/flask/app.py
+++ 3_conn_containers/flask/app.py
@@ -7,10 +7,10 @@ from flask_mysqldb import MySQL
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-app.config['MYSQL_HOST'] = 'host.docker.internal'
+app.config['MYSQL_HOST'] = 'mysql_api_container'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'flaskhost'
+app.config['MYSQL_DB'] = 'flaskdocker'
 
 mysql = MySQL(app)
 
